a6096e34cc41f1a65292c96013bcd9441b806067
Julie Evans <juliemevans17@gmail.com>
2022-12-28 15:36:59 -0600

    Added zero padding to numbers

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function PersonComponent({ pokemon, open, onOpen }) {
-  const [sprites, setSprites] = React.useState("")
   const [info, setInfo] = React.useState(null)
 
   React.useEffect(() => {
@@ -11,19 +9,26 @@ export default function PersonComponent({ pokemon, open, onOpen }) {
       fetch(pokemon.url)
       .then((r) => r.json())
       .then((data) => {
-        // setSprites(data.sprites)
         setInfo(data)
       })
     }
   }, [pokemon])
 
-  // console.log(info)
+  function padZero(id) {
+    if(id <= 9) {
+      return "00" + id
+    }
+    else if(id >= 9 && id <= 99) {
+      return "0" + id
+    }
+    else return id
+  }
 
   return (
     <li className={styles.card} onClick={() => onOpen(info.id)}>
       <img src={info ? info.sprites.front_default : ""} width={open ? "200px" : ""} />
       <h3>{pokemon.name}</h3>
-      {info ? <p>{info.id}</p> : <></>}
+      {info ? <p>{`#${padZero(info.id)}`}</p> : <></>}
       <div className={styles.row}>
         {info && open ? (
           info.types.map((type) => <p>{type.type.name}</p>)
