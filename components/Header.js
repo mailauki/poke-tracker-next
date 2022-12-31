4adb1f5d06d08bbea270c2416fb4870959e1e493
Julie Evans <juliemevans17@gmail.com>
2022-12-30 19:09:11 -0600

    Updated header to MUI

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,93 @@
+import React from 'react'
 import styles from '../styles/Home.module.css'
 import Pokeball from '../components/icons/Pokeball'
-import { Box, IconButton, Typography } from '@mui/material'
+import { Box, Typography, AppBar, Toolbar, Button, IconButton, Menu, MenuItem, Tooltip, Avatar } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
+import MenuIcon from '@mui/icons-material/Menu'
 
 export default function Header({ open, onOpen }) {
-  return (
-    <header className={styles.header}>
-      {!open ? (
-        <Box className={styles.title}>
-          <Pokeball size="2rem" />
+  
+  // const settings = ['Profile', 'Account', 'Dashboard', 'Logout']
+  // const [anchorEl, setAnchorEl] = React.useState(null)
 
-          <Typography variant="h4">
-            PokeTracker
-          </Typography>
-        </Box>
-      ) : (
-        <IconButton onClick={() => onOpen(false)}>
-          <ArrowBackIosNewIcon />
-        </IconButton>
-      )}
-    </header>
+  // function handleOpenMenu(e) {
+  //   setAnchorEl(e.currentTarget)
+  // }
+
+  // function handleCloseMenu() {
+  //   setAnchorEl(null)
+  // }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar 
+        position="fixed" 
+        sx={{ color: "inherit" }}
+        className={styles.header}
+      >
+        <Toolbar>
+          {open ? (
+            <IconButton onClick={() => onOpen(false)}>
+              <ArrowBackIosNewIcon />
+            </IconButton>
+          ) : (
+            <Box className={styles.logo}>
+              <Box sx={{ display: { xs: "flex", md: "none" }, mr: 0.75 }}>
+                <Pokeball size={30} />
+              </Box>
+              
+              <Typography
+                variant="h5"
+                noWrap
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  fontFamily: "monospace",
+                  fontWeight: 600,
+                  letterSpacing: ".02rem",
+                  color: "inherit",
+                  textDecoration: "none"
+                }}
+              >
+                PokeTracker
+              </Typography>
+            </Box>
+          )}
+
+          <Box sx={{ flexGrow: 1 }}>
+          </Box>
+
+          {/* <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open Settings">
+              <IconButton onClick={handleOpenMenu} sx={{ p: 0 }}>
+                <Avatar />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
